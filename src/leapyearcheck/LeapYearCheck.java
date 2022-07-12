a3369ae7d00a3754484a4afc18ee53b0578b1c3d
Kevin Bell <bellKevin@pm.me>
2022-07-12 00:19:39 +0000

    Update LeapYearCheck.java

--- src/leapyearcheck/LeapYearCheck.java
+++ src/leapyearcheck/LeapYearCheck.java
@@ -1,70 +1,46 @@
-package leapyearcheck;
-
-/******************************************************************************
- * LeapYearCheck.java 
- * Programmer: @author jcboyd
- * Version: 1.0
- * Course: SDEV 1060
+/** ****************************************************************************
+ * LeapYearCheck.java
+ * Kevin Bell
  *
- * This program asks for the user to input a year and returns the number of 
+ * This program asks for the user to input a year and returns the number of
  * days in February and if the year is a leap year
- *****************************************************************************/
+ **************************************************************************** */
+package leapyearcheck;
 
 import java.util.*; //Scanner
 
 public class LeapYearCheck {
 
-    public static int daysInFebruary(int year)
-    {
-        if ((year % 100 == 0) && (year % 400 == 0))
-        {
+    public static int daysInFebruary(int year) {
+        if ((year % 100 == 0) && (year % 400 == 0)) {
             return 29;
-        }
-        else 
-            if ((year % 100 != 0) && (year % 4 == 0))
-            {
-                return 29;
-            }
-            else
-            {
-                return 28;
-            } //end if
+        } else if ((year % 100 != 0) && (year % 4 == 0)) {
+            return 29;
+        } else {
+            return 28;
+        } //end if
     } //end daysInFebruary
-    
-    public static boolean isLeapYear(int year)
-    {
-        if ((year % 100 == 0) && (year % 400 == 0))
-        {
+
+    public static boolean isLeapYear(int year) {
+        if ((year % 100 == 0) && (year % 400 == 0)) {
             return true;
-        }
-        else
-            if ((year % 100 != 0) && (year % 4 == 0))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            } //end if
+        } else if ((year % 100 != 0) && (year % 4 == 0)) {
+            return true;
+        } else {
+            return false;
+        } //end if
     } //end isLeapYear
-    
+
     public static void main(String[] args) {
-        
-        Scanner stdIn = new Scanner(System.in);
-        
-        System.out.println("Leap Year Check by JC Boyd\n");
+        Scanner computerKeyboardInput = new Scanner(System.in);
+        System.out.println("Leap Year Check by Kevin Bell\n");
         System.out.print("Enter year: ");
-        int year = Integer.parseInt(stdIn.nextLine());
+        int year = Integer.parseInt(computerKeyboardInput.nextLine());
         System.out.println("The number of days in February is " + daysInFebruary(year));
-        if (isLeapYear(year))
-        {
+        if (isLeapYear(year)) {
             System.out.println("This year IS a leap year");
-        }
-        else
-        {
+        } else {
             System.out.println("This year is NOT a leap year");
         }
     } //end main
-    
 } //end class LeapYearCheck
-
